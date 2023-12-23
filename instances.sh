55bfa07d94adce9a7be4aa5511f6b95bb0cb80ad
sairam <sairam.sai.t7@gmail.com>
2023-12-23 11:03:00 +0530

    instance creation

--- instances.sh
+++ instances.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
-
-Image_id = $(ami-03265a0778a880afb)
-SG_id = $(sg-0c954e6aa75dd655c)
-Instances = ["web","Redis","Mysql"]
+Images_id=ami-03265a0778a880afb
+SG_id=sg-0c954e6aa75dd655c
+Instances = ("web","Redis","Mysql")
 
 for i in "${instances[@]}"
 do
