5f2433faf5c1ec41d085394ad0e2c64164bdd9fe
sairam <sairam.sai.t7@gmail.com>
2023-12-23 11:34:56 +0530

    instance creation

--- echo.sh
+++ echo.sh
@@ -11,4 +11,4 @@ fi
 
 #yum install mysql -y
 
-aws ec2 run-instances --image-id vami-03265a0778a880afb --count 1 --instance-type t2.micro --security-group-ids sg-0c954e6aa75dd655c
+aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type t2.micro --security-group-ids sg-0c954e6aa75dd655c
