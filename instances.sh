837b0360a829f9c1fe93dc3356be1e4378456e8d
sairam <sairam.sai.t7@gmail.com>
2023-12-23 10:58:21 +0530

    instance creation

--- instances.sh
+++ instances.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type t2.micro --security-group-ids sg-0c954e6aa75dd655c
+
 
 Image_id = $(ami-03265a0778a880afb)
 SG_id = $(sg-0c954e6aa75dd655c)
 Instances = ["web","Redis","Mysql"]
 
-for i in instances[@]
+for i in "${instances[@]}"
 do
-  if [ i == "Mysql"] || [i == "redis"]
+  if [ $i == "Mysql" ] || [ $i == "Redis" ]
    then
-        instance-type = "t3.small"
+        instance_type = "t3.small"
      else
-        instance-type = "t2.micro"
+        instance_type = "t2.micro"
    fi 
-   aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type t2.micro --security-group-ids sg-0c954e6aa75dd655c
+   aws ec2 run-instances --image-id $Image_id --count 1 --instance-type t2.micro --security-group-ids $SG_id
 done       
